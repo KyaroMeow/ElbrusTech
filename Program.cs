3396c82624e6f399b3e50d49d10cce674fdeded5
diemeoow <exxxxxxe90@gmail.com>
2024-04-20 21:58:54 +0300

    1001

--- Program.cs
+++ Program.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Data.SQLite;
+using System.Configuration;
 
 namespace ElbrusTech
 {
 	internal static class Program
 	{
-		/// <summary>
-		/// Главная точка входа для приложения.
-		/// </summary>
-		[STAThread]
+
 		static void Main()
 		{
+			//string connectionString = ConfigurationManager.ConnectionStrings["SQLiteConnection"].ConnectionString;
+			//using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+			//{
+			//	connection.Open();
+			//	// Ваш код для работы с базой данных
+			//	connection.Close();
+			//}
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
