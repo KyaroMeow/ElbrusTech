17d5abeeec94739f37eb679f3fe996e254577257
KyaroMeow <kirillid2006@gmail.com>
2024-04-20 03:45:45 +0300

    creating library

--- Form1.cs
+++ Form1.cs
@@ -1,21 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
+﻿using System.Windows.Forms;
+using LibraryForET;
 namespace ElbrusTech
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            Methods methods = new Methods();
+            try
+            {
+                methods.GenereticEmployees(3);
+                MessageBox.Show("все хорошо");
+            }
+            catch
+            {
+                MessageBox.Show("все полхо");
+            }
+        }
     }
 }
